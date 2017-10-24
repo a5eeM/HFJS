febfba9590f67a17164852cc19a051ddf22e71da
Aseem Sharma <30532759+a5eeM@users.noreply.github.com>
2017-10-25 00:40:56 +0530

    Add Ch 07 code

---
+++ Ch 07/emot.js
@@ -0,0 +1,16 @@
+var emot = "XOxxOO";
+var hugs = 0;
+var kisses = 0;
+
+emot = emot.trim();
+emot = emot.toUpperCase();
+
+for (var i = 0; i < emot.length; i++) {
+    if (emot.charAt(i) === "X") {
+        hugs++;
+    } else if (emot.charAt(i) === "O") {
+        kisses++;
+    }
+}
+
+console.log("You gave me " + hugs + " hugs & " + kisses + " kisses ?");
