adb2f334a691b8128ca329d5cd8f20da760e6018
Aseem Sharma <30532759+a5eeM@users.noreply.github.com>
2017-10-30 01:02:56 +0530

    Add Ch 08 code

--- Ch 08/battleship.js
+++ Ch 08/battleship.js
@@ -15,55 +15,6 @@ var view = {
     }
 };
 
-/*
-// testing view
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Tap tap, is this thing on?")
-*/
-
-/*
-// Ship objects
-var ship1 = {
-    locations: ["10", "20", "30"],
-    hits: ["", "", ""]
-};
-
-var ship2 = {
-    locations: ["22", "32", "34"],
-    hits: ["", "", ""]
-};
-
-var ship3 = {
-    locations: ["63", "64", "65"],
-    hits: ["", "", ""]
-};
-*/
-
-/*
-// this ships array will be added to model object as a property!
-var ships = [
-    {
-        locations: ["10", "20", "30"],
-        hits: ["", "", ""]
-    },
-    
-    {
-        locations: ["32", "33", "34"],
-        hits: ["", "", ""]
-    },
-    
-    {
-        locations: ["63", "64", "65"],
-        hits: ["", "", "hit"]
-    }
-];
-*/
-
 var model = {
     boardSize: 7,
     numShips: 3,
@@ -92,7 +43,11 @@ var model = {
             var ship = this.ships[i];
             //var locations = ship.locations;
             var index = ship.locations.indexOf(guess);
-            if (index >= 0) {
+            
+            if (ship.hits[index] === "hit") {
+                view.displayMessage("Oops, you already hit that location!");
+                return true; // important
+            } else if (index >= 0) {
                 ship.hits[index] = "hit";
                 
                 // notify the view that we got a hit at the location in guess
@@ -172,22 +127,6 @@ var model = {
     }
 };
 
-/*
-// testing model
-model.fire("53");
-
-model.fire("06");
-model.fire("16");
-model.fire("26");
-
-model.fire("34");
-model.fire("24");
-model.fire("44");
-
-model.fire("12");
-model.fire("11");
-model.fire("10");
-*/
 
 var controller = {
     guesses: 0,
@@ -207,6 +146,8 @@ var controller = {
     }
 };
 
+
+// helper function to parse a guess from the user
 function parseGuess(guess) {
     // helper array
     var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
@@ -230,42 +171,8 @@ function parseGuess(guess) {
     return null;
 }
 
-/*
-// test parseGuess
-console.log(parseGuess("A0"));
-console.log(parseGuess("B6"));
-console.log(parseGuess("G3"));
-console.log(parseGuess("H0"));
-console.log(parseGuess("A7"));
 
-*/
-
-/*
-// test controller
-controller.processGuess("A0");
-
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
-
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
-*/
-
-function init() {
-    var fireButton =  document.getElementById("fireButton");
-    fireButton.onclick = handleFireButton;
-    var guessInput = document.getElementById("guessInput");
-    guessInput.onkeypress = handleKeyPress;
-    
-    model.generateShipLocations();
-}
-
+// event handlers
 function handleFireButton() {
     var guessInput = document.getElementById("guessInput");
     var guess = guessInput.value;
@@ -284,4 +191,15 @@ function handleKeyPress(e) {
     }
 }
 
+// init - called when the page completes loading
 window.onload = init;
+
+function init() {
+    var fireButton =  document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+    
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+    // place ships on board
+    model.generateShipLocations();
+}
